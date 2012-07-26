e90947f8ec17b31a5e9c6bf5b1d871bde679adea
Serge Matveenko <serge@etsoft.ru>
2012-07-26 14:05:36 +0400

    Add comments into post document.

--- core/documents.py
+++ core/documents.py
@@ -5,12 +5,20 @@ from mongoengine import *
 from mongoengine.django.auth import User
 
 
+class Comment(EmbeddedDocument):
+
+    user = ReferenceField(User, required=True)
+    created = DateTimeField()
+    text = StringField(required=True)
+
+
 class Post(Document):
 
     user = ReferenceField(User, required=True)
     created = DateTimeField()
     updated = DateTimeField()
     text = StringField(required=True)
+    comments = SortedListField(EmbeddedDocumentField(Comment))
 
     meta = {
         'ordering': ['-created']
@@ -19,12 +27,18 @@ class Post(Document):
     def save(self, *args, **kwargs):
         now = datetime.utcnow()
 
-        if not self.pk:
-            self.created = self.updated = now
-        elif any(self._delta()):
+        if not self.created:
+            self.created = now
+
+        if any(self._delta()):
             self.updated = now
 
-        super(Post, self).save()
+        for i in range(len(self.comments)):
+
+            if not self.comments[i].created:
+                self.comments[i].created = now
+
+        super(Post, self).save(*args, **kwargs)
 
     @permalink
     def get_absolute_url(self):
